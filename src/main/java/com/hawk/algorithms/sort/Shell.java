b26b510f1d652f04c54bf4f63c68a92edcc66c5e
HAWK97 <wsghawk97@163.com>
2018-09-06 20:27:07 +0800

    ADD:希尔排序

---
+++ src/main/java/com/hawk/algorithms/sort/Shell.java
@@ -0,0 +1,22 @@
+package com.hawk.algorithms.sort;
+
+public class Shell {
+    private static boolean less(Comparable v, Comparable w) {
+        return v.compareTo(w) < 0;
+    }
+
+    private static void exch(Comparable[] a, int i, int j) {
+        Comparable t = a[i];
+        a[i] = a[j];
+        a[j] = t;
+    }
+
+    private static void show(Comparable[] a) {
+        for (int i = 0; i < a.length; i++) {
+            System.out.print(a[i] + " ");
+        }
+        System.out.println();
+    }
+
+
+}
