a163adcc649dfdac9df501e13ea425f495cc67cd
HAWK97 <wsghawk97@163.com>
2018-10-14 14:30:06 +0800

    ADD:将输入的字符串反转过来

--- src/main/java/com/hawk/leetCode/string/ReverseString344.java
+++ src/main/java/com/hawk/leetCode/string/ReverseString344.java
@@ -1,6 +1,10 @@
 package com.hawk.leetCode.string;
 
-public class reverseString {
+/**
+ * 题目描述：编写一个函数，其作用是将输入的字符串反转过来
+ * 示例：输入："hello" 输出: "olleh"
+ */
+public class ReverseString344 {
 
     /**
      * 我的答案
@@ -14,11 +18,24 @@ public class reverseString {
     }
 
     /**
-     * 网上答案
+     * 网上答案（StringBuilder）
      */
     public String reverseString2(String s) {
         StringBuilder ans = new StringBuilder(s);
         // 直接用reverse方法，更简单
         return ans.reverse().toString();
+    }
+
+    /**
+     * 网上答案（char数组）
+     */
+    public String reverseString3(String s) {
+        char[] chars = s.toCharArray();
+        for (int i = 0; i < chars.length / 2; i++) {
+            char tmp = chars[i];
+            chars[i] = chars[chars.length - 1 - i];
+            chars[chars.length - 1 - i] = tmp;
+        }
+        return new String(chars);
     }
 }
