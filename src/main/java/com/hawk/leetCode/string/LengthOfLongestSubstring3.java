09a98a926dd33f57b42d9b8eae52c43ba7b87490
HAWK97 <wsghawk97@163.com>
2018-10-15 20:04:24 +0800

    MOD:给定一个字符串，找出不含有重复字符的最长子串的长度

--- src/main/java/com/hawk/leetCode/string/LengthOfLongestSubstring3.java
+++ src/main/java/com/hawk/leetCode/string/LengthOfLongestSubstring3.java
@@ -26,12 +26,11 @@ public class LengthOfLongestSubstring3 {
         Set<Character> set = new HashSet<>();
         int ans = 0, i = 0, j = 0;
         while (i < n && j < n) {
-            if (!set.contains(s.charAt(j))){
+            if (!set.contains(s.charAt(j))) {
                 // 首先执行 set.add(s.charAt(j))，再 j++
                 set.add(s.charAt(j++));
                 ans = Math.max(ans, j - i);
-            }
-            else {
+            } else {
                 set.remove(s.charAt(i++));
             }
         }
