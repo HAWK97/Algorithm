74863fd633c882e03e682f727878cc8cfb214925
HAWK97 <wsghawk97@163.com>
2018-10-16 00:15:36 +0800

    ADD:给定一个字符串，找出不含有重复字符的最长子串的长度

--- src/main/java/com/hawk/leetCode/string/FirstUniqChar387.java
+++ src/main/java/com/hawk/leetCode/string/FirstUniqChar387.java
@@ -1,4 +1,24 @@
 package com.hawk.leetCode.string;
 
+/**
+ * 题目描述：给定一个字符串，找出不含有重复字符的最长子串的长度
+ * 示例：s = "leetcode"，返回 0；s = "loveleetcode"，返回 2
+ */
 public class FirstUniqChar387 {
+
+    /**
+     * 网上答案
+     */
+    public int firstUniqChar(String s) {
+        int[] freq = new int[26];
+        for (int i = 0; i < freq.length; i++) {
+            freq[s.charAt(i) - 'a']++;
+        }
+        for (int i = 0; i < freq.length; i++) {
+            if (freq[s.charAt(i) - 'a'] == 1) {
+                return i;
+            }
+        }
+        return -1;
+    }
 }
