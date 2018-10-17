af1cdea8256ff4729e374658fdca62ded4647881
HAWK97 <wsghawk97@163.com>
2018-10-17 14:04:30 +0800

    MOD:给定一个字符串，找到它的第一个不重复的字符，并返回它的索引

--- src/main/java/com/hawk/leetCode/string/FirstUniqChar387.java
+++ src/main/java/com/hawk/leetCode/string/FirstUniqChar387.java
@@ -1,7 +1,7 @@
 package com.hawk.leetCode.string;
 
 /**
- * 题目描述：给定一个字符串，找出不含有重复字符的最长子串的长度
+ * 题目描述：给定一个字符串，找到它的第一个不重复的字符，并返回它的索引。如果不存在，则返回 -1
  * 示例：s = "leetcode"，返回 0；s = "loveleetcode"，返回 2
  */
 public class FirstUniqChar387 {
