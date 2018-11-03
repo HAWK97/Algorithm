97bd97af0308afce3f228be3ab1bba4e048c17d7
HAWK97 <wsghawk97@163.com>
2018-11-03 16:37:32 +0800

    ADD:实现一个将字符串进行指定行数变换的函数

---
+++ src/main/java/com/hawk/leetCode/string/Convert6.java
@@ -0,0 +1,46 @@
+package com.hawk.leetCode.string;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 题目描述：将字符串 "PAYPALISHIRING" 以Z字形排列成给定的行数：
+ * P   A   H   N
+ * A P L S I I G
+ * Y   I   R
+ * 之后从左往右，逐行读取字符："PAHNAPLSIIGYIR"
+ * 实现一个将字符串进行指定行数变换的函数
+ * 示例：如上
+ */
+public class Convert6 {
+
+    /**
+     * 按行排序
+     * 解题思路：我们可以使用大小为 min(numRows, len(s)) 的列表来表示 Z 字形图案中的非空行
+     * 从左到右迭代 s，将每个字符添加到合适的行。可以使用当前行和当前方向这两个变量对合适的行进行跟踪
+     * 只有当我们向上移动到最上面的行或向下移动到最下面的行时，当前方向才会发生改变
+     */
+    public String convert(String s, int numRows) {
+        if (numRows == 1) {
+            return s;
+        }
+        List<StringBuilder> rows = new ArrayList<>();
+        for (int i = 0; i < Math.min(numRows, s.length()); i++) {
+            rows.add(new StringBuilder());
+        }
+        int curRow = 0;
+        boolean goingDown = false;
+        for (char c : s.toCharArray()) {
+            rows.get(curRow).append(c);
+            if (curRow == 0 || curRow == numRows - 1) {
+                goingDown = !goingDown;
+            }
+            curRow += goingDown ? -1 : 1;
+        }
+        StringBuilder result = new StringBuilder();
+        for (StringBuilder row : rows) {
+            result.append(row);
+        }
+        return result.toString();
+    }
+}
