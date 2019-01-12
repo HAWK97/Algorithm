79e4933485463903b5afec2c6f9afc604c72d052
Hawk97 <wsghawk97@163.com>
2019-01-13 01:02:59 +0800

    ADD:对于一个给定的字符序列 S，把其循环左移 K 位后的序列输出

---
+++ src/main/java/com/hawk/swordoffer/string/LeftRotateString.java
@@ -0,0 +1,31 @@
+package com.hawk.swordoffer.string;
+
+/**
+ * 题目描述：对于一个给定的字符序列 S，请你把其循环左移 K 位后的序列输出
+ */
+public class LeftRotateString {
+
+    public String LeftRotateString(String str,int n) {
+        n %= str.length();
+        char[] chars = str.toCharArray();
+        reverse(chars, 0, n - 1);
+        reverse(chars, n, chars.length - 1);
+        reverse(chars, 0, chars.length - 1);
+        StringBuilder sb = new StringBuilder();
+        for (char c : chars) {
+            sb.append(c);
+        }
+        return sb.toString();
+    }
+
+    private void reverse(char[] chars, int start, int end) {
+        char temp;
+        while (start < end) {
+            temp = chars[start];
+            chars[start] = chars[end];
+            chars[end] = temp;
+            start++;
+            end--;
+        }
+    }
+}
