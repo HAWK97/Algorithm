365db854416d2f38941bb1573b61d0c335679066
Hawk97 <wsghawk97@163.com>
2019-02-14 16:20:31 +0800

    MOD:求给定数组中不相邻数能形成的最大和

--- src/main/java/com/hawk/leetCode/array/Rob198.java
+++ src/main/java/com/hawk/leetCode/array/Rob198.java
@@ -27,7 +27,7 @@ public class Rob198 {
         // 初始化dp[0] = nums[0]，dp[1] = max(nums[0], nums[1])
         dp[0] = nums[0];
         dp[1] = Math.max(nums[0], nums[1]);
-        for (int i = 0; i < nums.length; i++) {
+        for (int i = 2; i < nums.length; i++) {
             dp[i] = Math.max(nums[i] + dp[i - 2], dp[i - 1]);
         }
         return dp[nums.length - 1];
